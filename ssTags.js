2d6d6f9e12478f9bdf2e0f46adc7818682d678ef
geraldgillespie <gerald.gillespie@inscio.com>
2022-06-17 18:25:30 -0400

    netsuite tags are now defined dynamically also

--- ssTags.js
+++ ssTags.js
@@ -1,9 +1,48 @@
+const env = require('jsdoc/env');
+const {suitescript} = env.conf;
+const withoutPartialTags = [
+  { label : 'Require Config', tag: 'NAmdConfig'},
+  { label: 'SuiteScript API', tag: 'NApiVersion'},
+  { label: 'Scope', tag : 'NModuleScope'},
+  { label : 'ScriptType' , tag : 'NScriptType'}
+];
+const reservedNames = withoutPartialTags.map(wpt=>wpt.tag).concat(['governance','appliedtorecord'] )
+const newTags = [...(suitescript?.basicTags || [])]
+  // filter out the ones defined above
+  .filter(candidate=>!reservedNames.includes(candidate))
+  .map(t=>({ label : t, tag : t }));
+
+const uniqueTags = [...newTags, ...withoutPartialTags].flat();
 /**
  * Adds support for several SuiteScript-related tags
  */
 exports.defineTags = function (dictionary) {
-  dictionary
-    .defineTag('governance', {
+  for(let i = 0, l=uniqueTags.length; i<=l; i++) {
+    const { label, tag : newTag} = typeof uniqueTags[i] === 'object' ? uniqueTags[i] : { label : uniqueTags[i], tag: uniqueTags[i] };
+    if (typeof newTag !== 'string') break;
+      try{
+        dictionary.defineTag(newTag, {
+          mustHaveValue : true,
+          canHaveType: false,
+          canHaveName: false,
+          'onTagged': function (doclet, tag){
+            doclet[newTag] = tag.value;
+            if( !doclet.meta){
+              doclet.meta = { customTagName : [] };
+            }
+            if( !doclet.meta.customTagName){
+              doclet.meta.customTagName = [];
+            }
+            // the customTagName must match the doclet key and tag above
+            doclet.meta.customTagName.push({ label, tag: newTag });
+          }
+        });
+      }catch(e){
+        //
+      }
+  };
+
+  dictionary.defineTag('governance', {
       'mustHaveValue': true,
       canHaveType: false,
       canHaveName: false,
@@ -17,62 +56,7 @@ exports.defineTags = function (dictionary) {
       }
     })
     .synonym('gov');
-  dictionary.defineTag('NScriptType', {
-    'mustHaveValue': true,
-    canHaveType: false,
-    canHaveName: false,
-    mustNotHaveDescription: true,
-    'onTagged': function (doclet, tag) {
-      doclet.NScriptType = tag.value;
 
-      if (!doclet.meta) {
-        doclet.meta = {};
-      }
-      doclet.meta.partial = 'nscripttype.tmpl';
-    }
-  });
-  dictionary.defineTag('NModuleScope', {
-    'mustHaveValue': true,
-    canHaveType: false,
-    canHaveName: false,
-    mustNotHaveDescription: true,
-    'onTagged': function (doclet, tag) {
-      doclet.NModuleScope = tag.value;
-
-      if (!doclet.meta) {
-        doclet.meta = {};
-      }
-      doclet.meta.partial = 'nmodulescope.tmpl';
-    }
-  });
-  dictionary.defineTag('NApiVersion', {
-    'mustHaveValue': true,
-    canHaveType: false,
-    canHaveName: false,
-    mustNotHaveDescription: true,
-    'onTagged': function (doclet, tag) {
-      doclet.NApiVersion = tag.value;
-
-      if (!doclet.meta) {
-        doclet.meta = {};
-      }
-      doclet.meta.partial = 'napiversion.tmpl';
-    }
-  });
-  dictionary.defineTag('NAmdConfig', {
-    'mustHaveValue': true,
-    canHaveType: false,
-    canHaveName: false,
-    mustNotHaveDescription: true,
-    'onTagged': function (doclet, tag) {
-      doclet.NAmdConfig = tag.value;
-
-      if (!doclet.meta) {
-        doclet.meta = {};
-      }
-      doclet.meta.partial = 'namdconfig.tmpl';
-    }
-  });
   dictionary.defineTag('appliedtorecord', {
     'mustHaveValue': true,
     canHaveType: false,
@@ -83,11 +67,6 @@ exports.defineTags = function (dictionary) {
       }
 
       doclet.appliedtorecord.push(tag.value);
-
-      if (!doclet.meta) {
-        doclet.meta = {};
-      }
-      doclet.meta.partial = 'appliedtorecord.tmpl';
     }
   })
   .synonym('appliestorecord');
